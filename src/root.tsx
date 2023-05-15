70f528687e8128f7bf8163e590e91674bf7fbedc
dyckdavid <dyckd27@gmail.com>
2023-05-15 10:03:23 -0600

    content update

--- src/root.tsx
+++ src/root.tsx
@@ -24,7 +24,7 @@ export default function Root() {
   return (
     <Html lang="en">
       <Head>
-        <Title>SolidStart - With TailwindCSS</Title>
+        <Title>Arco Trailers</Title>
         <Meta charset="utf-8" />
         <Meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
@@ -39,6 +39,7 @@ export default function Root() {
                 <li class={`border-b-2 ${active("/about")} mx-1.5 sm:mx-6`}>
                   <A href="/about">About</A>
                 </li>
+                
               </ul>
             </nav>
             <Routes>
---
+++ src/routes/test.tsx
@@ -0,0 +1,14 @@
+import { A } from "solid-start";
+
+
+export default function Home() {
+  return (
+    <main class="text-center mx-auto text-gray-700 p-4">
+      <h1 class="max-6-xs text-5xl text-sky-600 font-thin uppercase my-8">
+        Test
+      </h1>
+      
+      
+    </main>
+  );
+}
